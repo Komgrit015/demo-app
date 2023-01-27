d6815708e7bc3745f8048baae91ea9fdf07d92a7
Komgrit015 <ford_za007@hotmail.com>
2023-01-27 12:00:26 +0700

    learning vue condition

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,100 @@
 <template>
   <section>
-      <img :src="picture" :width="size" :height="size" ref="imageEl"> <br>
-      <!-- Nickname : <input type="text" v-on:input="setNickname"/> -->
+    <img :src="picture" :width="size" :height="size" ref="imageEl" /> <br />
+    <!-- Nickname : <input type="text" v-on:input="setNickname"/> -->
 
-      <form @submit.prevent="submitForm">
+    <!-- <form @submit.prevent="submitForm">
           <label>Nickname : </label>
-          <!-- <input type="text" v-on:input="setNickname" ref="nicknameEl"/> -->
+          <input type="text" v-on:input="setNickname" ref="nicknameEl"/>
           <input type="text" ref="nicknameEl"/>
           <button type="submit"> Submit </button>
-      </form>
+      </form> -->
 
-      <h1>Name : {{ getFullname() }}</h1>
-      <h1>Lastname {{ lastName }}</h1>
-      <h2>Nickname {{ nickname }}</h2>
-      <h1>Age {{ age }}</h1>
-
+    <h1>Name : {{ getFullname() }}</h1>
+    <h1>Lastname {{ lastName }}</h1>
+    <!-- <h2>Nickname {{ nickname }}</h2> -->
+    <h1>Age {{ age }}</h1>
+      {{ isVisible }}
+    <button @click="toggleVisible">{{ isVisible ? "Hide" : "Display" }} Detail</button>  
+    <article v-show="isVisible">
       <p>Address : <span v-html="address"></span></p>
-      <p>Social: <a :href="social">FaceBook</a></p> 
-      <p>Hobby</p>
-      <ul>
-        <li>{{ hobby[0] }}</li>
-        <li>{{ hobby[1] }}</li>
-        <li>{{ hobby[2] }}</li>
-
-      </ul>
-      <p>Data</p>
+      <p>Social: <a :href="social">FaceBook</a></p>
+      <p v-if="hobby.length === 0">No Hobby</p>
+      <div v-else>
+        <p>Hobby</p>
         <ul>
-          <li>Gender: {{ general.gender }}</li>
-          <li>Weight: {{ general.weight }} KG</li>
-          <li>Height: {{ general.height }} CM</li>
-          <li>Status: {{ general.status }}</li>
+          <li v-for="(item, index) in hobby" :key="index">{{ item }}</li>
+          <!-- <li>{{ hobby[0] }}</li>
+            <li>{{ hobby[1] }}</li>
+            <li>{{ hobby[2] }}</li> -->
         </ul>
-        <button v-on:click="showData">Click</button>
+      </div>
+      <p>Data</p>
+      <ul>
+        <!-- <li>Gender: {{ general.gender }}</li>
+            <li>Weight: {{ general.weight }} KG</li>
+            <li>Height: {{ general.height }} CM</li>
+            <li>Status: {{ general.status }}</li> -->
+        <li v-for="(item, key) in general" :key="key">{{ key }} - {{ item }}</li>
+      </ul>
+    </article>
+
+    <!-- event -->
+    <!-- <button v-on:click="showData">Click</button>
         <button @click="increment(10)">Add</button>
-        <button @click.middle="decrement">Down</button>
-
-
+        <button @click.middle="decrement">Down</button> -->
   </section>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      firstName:"Komgrit",
-      lastName:"Studio",
-      nickname:"",
-      age:27,
-      address:"<strong>Test</strong>",
+      firstName: "Komgrit",
+      lastName: "Studio",
+      nickname: "",
+      age: 27,
+      address: "<strong>Test</strong>",
       picture: "https://cdn-icons-png.flaticon.com/512/8893/8893995.png",
-      size:50,
+      size: 50,
       social: "https://www.flaticon.com",
-      hobby:["Test","Football","Music"],
-      general:{gender:"men", weight:70, height:170, status:false}
-    }
+      hobby: ["Test", "Football", "Music", "Music", "Music"],
+      general: { gender: "men", weight: 70, height: 170, status: false },
+      isVisible:false
+    };
   },
-  methods:{
-    getFullname(){
+  methods: {
+    getFullname() {
       // return this.firstName + this.lastName
-      return `${this.firstName} ${this.lastName}`
+      return `${this.firstName} ${this.lastName}`;
     },
-    showData(){
-      alert(this.firstName)
-    },
-    increment(value){
-      this.age+=value
-    },
-    decrement(){
-      this.age--
-    },
+    toggleVisible(){
+        this.isVisible = !this.isVisible;
+    }
+    // showData(){
+    //   alert(this.firstName)
+    // },
+    // increment(value){
+    //   this.age+=value
+    // },
+    // decrement(){
+    //   this.age--
+    // },
     // setNickname(event){
     //   // console.log(event.target.value)
     //   this.nickname = event.target.value
 
     // },
-    submitForm(){
-      // alert("Success")
-      // console.log(this.$refs.imageEl) ;
-      this.nickname = this.$refs.nicknameEl.value;
-    
-    }
-  }
-}
+    // submitForm(){
+    //   // alert("Success")
+    //   // console.log(this.$refs.imageEl) ;
+    //   this.nickname = this.$refs.nicknameEl.value;
+
+    // }
+  },
+};
 </script>
 
 <style>
-
 </style>
